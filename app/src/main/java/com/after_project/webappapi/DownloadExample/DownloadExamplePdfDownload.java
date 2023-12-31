908891ccd9086e48d3a75f0dba48903408b59ad1
thiagoschnell <78884351+thiagoschnell@users.noreply.github.com>
2023-12-31 00:54:13 -0300

    update

--- app/src/main/java/com/after_project/webappapi/DownloadExample/DownloadExamplePdfDownload.java
+++ app/src/main/java/com/after_project/webappapi/DownloadExample/DownloadExamplePdfDownload.java
@@ -1,50 +1,28 @@
 package com.after_project.webappapi;
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.pdf.PdfRenderer;
 import android.os.Bundle;
-import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.util.Base64;
 import android.webkit.ValueCallback;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.io.File;
+import java.io.IOException;
 public class DownloadExamplePdfDownload extends AppCompatActivity {
-    final int PERMISSION_REQUEST_CODE = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download_example_pdf_download);
         if(MyApp.getInstance().getWebAppStatus() == MyApp.WEBAPP_STATUS_LOAD_FINISHED) {
-            Boolean permission= isStoragePermissionGranted();
-            if(!permission){
-                Toast.makeText(this,"you need grant permission to continue",Toast.LENGTH_LONG).show();
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
-                }
-            }else{
-                Add_Loading_Text("permission success");
-                handleAfterPermissionGranted();
-            }
-        }else{
-            Add_Loading_Text("Webapp is not loaded.");
-        }
-    }
-    private void handleAfterPermissionGranted(){
-        Add_Loading_Text("isExternalStorageAvailable is " + isExternalStorageAvailable());
-        Add_Loading_Text("isExternalStorageReadOnly is " + isExternalStorageReadOnly());
-        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
-            Add_Loading_Text("unable to continue.");
-        }
-        else {
             Add_Loading_Text("Downloading pdf...");
             {
                 try {
@@ -53,9 +31,6 @@ public class DownloadExamplePdfDownload extends AppCompatActivity {
                                 @Override
                                 public void onReceiveValue(Object value) {
                                     try {
-                                        String filename = "SampleFile.pdf";
-                                        String filepath = "MyFileStorage";
-                                        File myExternalFile;
                                         JsonObject json = JsonParser.parseString((String) value).getAsJsonObject();
                                         JsonElement data = json.get("data");
                                         if (data != null) {
@@ -64,27 +39,28 @@ public class DownloadExamplePdfDownload extends AppCompatActivity {
                                             Add_Loading_Text("Saving pdf...");
                                             {
                                                 StorageManager storageManager = new StorageManager();
-                                                Add_Loading_Text("get file...");
-                                                //myExternalFile = new File(getExternalFilesDir(filepath), filename);
-                                                myExternalFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), filename);
-                                                Add_Loading_Text("save pdf to file...");
-                                                storageManager.saveFile(myExternalFile, downloaded_bytes);
-                                                Add_Loading_Text("opening pdf...");
+                                                File fileObject = getFileObject();
+                                                Add_Loading_Text("get internal file...");
+                                                File pdf_file = storageManager.getFile(fileObject, "my_downloads/SampleFile.pdf");
+                                                Add_Loading_Text("save pdf to the file...");
+                                                storageManager.saveFile(pdf_file, downloaded_bytes);
                                                 {
-                                                    Intent intent =  new Intent(Intent.ACTION_VIEW);
-                                                    intent.setPackage("com.google.android.apps.docs"); //specific package name to open pdf file
-                                                    intent.setDataAndType(Uri.fromFile(myExternalFile), "application/pdf");
-                                                    try {
-                                                        startActivity(intent);
+                                                    /*
+                                                    Below example is just for example purposes to display the pdf image
+                                                     */
+                                                    Add_Loading_Text("rendering pdf to bitmap...");
+                                                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                                                        ImageView imageView = findViewById(R.id.DownloadExamplePdfDownloadImageView);
+                                                        Bitmap bitmap = pdfToBitmap(pdf_file);
+                                                        imageView.setImageBitmap(bitmap);
                                                         Add_Loading_Text("Done.");
-                                                    }catch (Exception e){
-                                                        Add_Loading_Text("no app found to read the pdf");
-                                                        Snackbar.make(findViewById(android.R.id.content), "Please install Google Drive App.", Snackbar.LENGTH_INDEFINITE).show();
+                                                    }else{
+                                                        Add_Loading_Text("Requires Api 21 to render");
                                                     }
                                                 }
                                             }
                                         } else {
-                                            Add_Loading_Text("image download error");
+                                            Add_Loading_Text("pdf download error");
                                             JsonElement error = json.get("error");
                                             if (error != null && !error.isJsonNull()) {
                                                 Add_Loading_Text(error.getAsString());
@@ -101,56 +77,21 @@ public class DownloadExamplePdfDownload extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
+        }else{
+            Add_Loading_Text("Webapp is not loaded.");
         }
     }
-    private boolean isExternalStorageReadOnly() {
-        String extStorageState = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
-            return true;
-        }
-        return false;
-    }
-    private boolean isExternalStorageAvailable() {
-        String extStorageState = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
-            return true;
-        }
-        return false;
-    }
-    private boolean isStoragePermissionGranted() {
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (this.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_GRANTED) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        else { //permission is automatically granted on sdk<23 upon installation
-            return true;
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case PERMISSION_REQUEST_CODE:
-                if (Build.VERSION.SDK_INT >= 23) {
-                    for (int i = 0; i < permissions.length; i++) {
-                        String permission = permissions[i];
-                        int grantResult = grantResults[i];
-                        if (permission.equals(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                            if (grantResult == PackageManager.PERMISSION_GRANTED) {
-                                Add_Loading_Text("WRITE_EXTERNAL_STORAGE permission success");
-                                handleAfterPermissionGranted();
-                            } else {
-                                Add_Loading_Text("WRITE_EXTERNAL_STORAGE permission denied");
-                            }
-                        }
-                    }
-                }
-        }
-    }
+    @RequiresApi(21)
+    private Bitmap pdfToBitmap(File file) throws IOException {
+        ParcelFileDescriptor fd = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
+        PdfRenderer renderer = null;
+        renderer = new PdfRenderer(fd);
+        Bitmap bitmap = Bitmap.createBitmap(222, 222, Bitmap.Config.ARGB_4444);
+        PdfRenderer.Page page = null;
+        page = renderer.openPage(1);
+        page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+       return bitmap;
+    };
     void Add_Loading_Text(String text){
         runOnUiThread(new Runnable() {
             @Override
@@ -159,5 +100,15 @@ public class DownloadExamplePdfDownload extends AppCompatActivity {
                         .append("\n" + text);
             }
         });
+    }
+    private File getFileObject(){
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        File directory = contextWrapper.getDir(getFilesDir().getName(), Context.MODE_PRIVATE);
+        /**
+         *  Note that files created through a File object will only be accessible by your own application;
+         *  you can only set the mode of the entire directory, not of individual files.
+         * Apps require no extra permissions to read or write to the returned path, since this path lives in their private storage.
+         */
+        return directory;
     }
 }
